e5f2e687d91bbf052a7e21d9b9122abfabe04355
JackDarkWeb <jacknouatin@yahoo.fr>
2018-11-19 15:49:18 +0200

    update file

--- index.php
+++ index.php
@@ -1,2 +1,73 @@
-v
-v
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Une formation compelte sur Jquery"/>
+    <meta http-equiv="content-language" content="fr"/>
+    <link rel="stylesheet" href="css/styles.css"/>
+    <title>Maitriser Jquery</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+
+<div id="hide0">
+
+    <p>
+        0 Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.
+    </p>
+    <button class="nextfiled" id="0">Next</button>
+</div>
+<div id="hide1">
+    <p>
+       1 Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.
+    </p>
+    <button class="nextfiled" id="1">Next</button>
+</div>
+<div id="hide2">
+    <p>
+        2 Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.
+    </p>
+    <button class="nextfiled" id="2">Next</button>
+</div>
+<div id="hide3">
+    <p>
+        3 Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.Le principe de fonctionnement d'XMLHttpRequest est d'envoyer une requête HTTP vers le serveur,
+        et une fois la requête envoyée, les données renvoyées par le serveur peuvent être récupérées. Pour ce faire,
+        il faut disposer d'un objet disposant de cette fonctionnalité. Cet objet a été développé par Microsoft et implémenté
+        dans Outlook puis dans Internet Explorer
+        5.5 en tant que contrôle ActiveX. Microsoft l'avait à l'époque nommé XMLHTTP.
+    </p>
+    <button class="nextfiled" id="3">Next</button>
+</div>
+
+<script src="hideShow.js"></script>
+</body>
+</html>
